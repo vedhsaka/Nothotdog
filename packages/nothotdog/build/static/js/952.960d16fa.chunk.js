aaf44f5551dbbb35b7aa95933ad639267e70ad7d
Neha Suresh <mailnehasuresh@gmail.com>
2024-08-03 02:18:31 -0700

    Retaining needed files

--- packages/nothotdog/build/static/js/952.960d16fa.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunknothotdog=self.webpackChunknothotdog||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.960d16fa.chunk.js.map
