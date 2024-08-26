1c92f0e8d536f775323ffc3b1590e556fc6b0d76
Neha Suresh <mailnehasuresh@gmail.com>
2024-08-25 21:24:35 -0700

    Removes ONE missing log

--- packages/nothotdog/src/components/APIConnectionForm.js
+++ packages/nothotdog/src/components/APIConnectionForm.js
@@ -132,7 +132,6 @@ const APIRequestForm = ({ onApiResponse, setOutputValue, onFullApiResponse, init
   };
 
   const handleSetOutputValue = (value, key) => {
-    console.log('Setting handleSetOutput');
     setSelectedNodePath(key);
     const cleanedKey = cleanValue(key);
     if (typeof cleanedKey === 'object') {
